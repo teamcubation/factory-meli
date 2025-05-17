e0bc218b54fe8d07b4d8eae880faae9b9312866d
luifiller <luiz.fsilva@sptech.school>
2025-05-16 21:33:03 -0300

    feat: add tweet and user models/strcuts

---
+++ 07-twitter/core/models/tweet.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Tweet struct {
+	ID        uuid.UUID `json:"id" validate:"required,gt=0"`
+	UserID    int       `json:"user_id" validate:"required,gt=0"`
+	Content   string    `json:"content" validate:"required,max=280"`
+	CreatedAt time.Time `json:"created_at" validate:"required"`
+}
---
+++ 07-twitter/core/models/user.go
@@ -0,0 +1,9 @@
+package models
+
+import "github.com/google/uuid"
+
+type User struct {
+	ID        uuid.UUID `json:"id" validate:"required,gt=0"`
+	Username  string    `json:"username" validate:"required,min=3,max=50"`
+	Following []int     `json:"following"`
+}
