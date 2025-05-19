a39a8bf44b9ecc6de1d5c44eda24dfd349afde69
Luiz Nison Filler <111442355+luifiller@users.noreply.github.com>
2025-05-19 17:04:44 -0300

    refactor: add nil validation to Trainer AddToParty()
    Co-authored-by: Damian Marquez <marquez.damian@outlook.com>

--- 04-structs/models/trainers.go
+++ 04-structs/models/trainers.go
@@ -14,9 +14,14 @@ const MAX_PARTY_SIZE int = 6
 
 
 func (t *Trainer) AddToParty(p *Pokemon) error {
+	if p == nil {
+		return errors.New("pokemon não pode ser nulo")
+	}
+
 	if len(t.Party) >= MaxPartySize {
 		return errors.New("o time do treinador já está cheio")
 	}
+
 
 	if t.Party == nil {
 		t.Party = make([]*Pokemon, 0, MaxPartySize)
