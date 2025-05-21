eb880fd91c75c2bb946eb33528fa432641c77584
Yuri Padlipskas <yupadlipskas@gmail.com>
2025-05-21 13:41:43 -0300

    Refactor: Update CreateTweetDTORequest and add TweetsByUserDTOResponse and UserActionRequestDTO for improved data handling

--- twitter-yuri/application/tweet/dtos/createTweetDTORequest.go
+++ twitter-yuri/application/tweet/dtos/createTweetDTORequest.go
@@ -2,18 +2,20 @@ package dtos
 
 import (
 	"Yuri/twitter/core/models"
+	"time"
 )
 
 type CreateTweetDTORequest struct {
-	Title       string      `json:"title" binding:"required"`
-	Description string      `json:"description" binding:"required"`
-	Author      models.User `json:"author" binding:"required"`
+	Title       string `json:"title" binding:"required"`
+	Description string `json:"description" binding:"required,max=280"`
+	AuthorId    string `json:"author_id" binding:"required"`
 }
 
 func ToModel(dto *CreateTweetDTORequest) *models.Tweet {
 	return &models.Tweet{
 		Title:       dto.Title,
 		Description: dto.Description,
-		Author:      dto.Author,
+		AuthorId:    dto.AuthorId,
+		CreatedAt:   time.Now(),
 	}
 }
